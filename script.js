809fed938a98a6746d1deca6b4e2ac368172b8ae
wgetgoose <louiscarsonferreira@gmail.com>
2022-03-14 01:11:24 -0700

    Media tags for small devices && centered bubbles

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const smallMediaQuery = window.matchMedia('(max-width: 1279px');
 smallMediaQuery.addEventListener("resize", dyanmicMiddleText());
 
-
 dyanmicMiddleText();
 function dyanmicMiddleText() {
-    console.log(smallMediaQuery.matches)
     if (smallMediaQuery.matches) {
         let right = document.getElementById('rightText');
         right.textContent = "Orangebytes works by allowing you to direct access to a skilled technician who has the ability to solve a broad range of tech-related issues."
+        const paras = document.getElementsByClassName('circleParagraph');
+        while(paras[0]) {
+            paras[0].parentNode.removeChild(paras[0])
+        };
+        const circles = Array.from(document.getElementsByClassName('invisibleBox'));
+        circles.forEach(element => {
+            element.style.cssText = "display: flex; align-items: center;"
+        });
         return;
     }
-    else {return;}
 }
