52c4e06f09cf1800bf906de8d4eed3f76a8725a5
wgetgoose <louiscarsonferreira@gmail.com>
2022-03-14 00:15:53 -0700

    Continued progress on home page media tags

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const smallMediaQuery = window.matchMedia('(max-width: 1279px')
+const smallMediaQuery = window.matchMedia('(max-width: 1279px');
+smallMediaQuery.addEventListener("resize", dyanmicMiddleText());
 
-if (smallMediaQuery.matches) {
+
+dyanmicMiddleText();
+function dyanmicMiddleText() {
+    console.log(smallMediaQuery.matches)
+    if (smallMediaQuery.matches) {
+        let right = document.getElementById('rightText');
+        right.textContent = "Orangebytes works by allowing you to direct access to a skilled technician who has the ability to solve a broad range of tech-related issues."
+        return;
+    }
+    else {return;}
 }
