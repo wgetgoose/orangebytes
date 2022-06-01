9d895a9a17bfdd0cf0215780961fafcf0d96356a
wgetgoose <louiscarsonferreira@gmail.com>
2022-05-31 18:11:02 -0700

    added reinsertion of content after resizing

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 const smallMediaQuery = window.matchMedia('screen and (max-width: 1279px)');
-smallMediaQuery.addEventListener("resize", dyanmicMiddleText());
+window.addEventListener('resize', dyanmicMiddleText)
 
 dyanmicMiddleText();
 function dyanmicMiddleText() {
+    let right = document.getElementById('rightText');
+    console.log("running");
+    const paras = Array.from(document.getElementsByClassName('circleParagraph'));
+    const circles = Array.from(document.getElementsByClassName('invisibleBox'));
     if (smallMediaQuery.matches) {
-        let right = document.getElementById('rightText');
+        console.log("matches")
         right.textContent = "Orangebytes works by allowing you to direct access to a skilled technician who has the ability to solve a broad range of tech-related issues."
-        const paras = document.getElementsByClassName('circleParagraph');
-        while(paras[0]) {
-            paras[0].parentNode.removeChild(paras[0])
-        };
-        const circles = Array.from(document.getElementsByClassName('invisibleBox'));
+        paras.forEach(element => {
+            element.style.cssText = "display: none"
+        });
         circles.forEach(element => {
             element.style.cssText = "display: flex; align-items: center;"
         });
         return;
     };
+    right.textContent =
+
+    `Orangebytes is a volunteer organization run by Carson Ferreira. We strive to
+    help educate the elderly residents of Orange County about our new world run by technology.
+    Sending emails, explaining what "the cloud" even is, and setting up Wi-Fi are all services we offer.
+    If you have any other problems, just email us! We're more than happy to help. Make sure to read more below.`
+    
+    paras.forEach(e => {e.style.cssText = "display: block"});
+    circles.forEach(e => {e.style.cssText = "display: flex; justify-content: center;"});
 };
 
 let button = document.getElementsByClassName("contactUs");
